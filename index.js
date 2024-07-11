fc37ea1d4a9ab3a0a747dcea2e2fa628b785932b
Khai Tran <khaitran123@gmail.com>
2024-07-10 21:13:42 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, type) {
+    let message = [];
+    for (let i = 0;
+        i < names.length;
+        i++)
+    {
+        message.push (`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return message
+}
+
+function countDown (n) {
+    let x = -1;
+    while (n > x){
+        console.log(n--)
+    }
+}
